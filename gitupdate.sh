61fff3313c6290044aba9740eba2d0f2954850e1
vicky4050 <arunrajappan@gmail.com>
2018-05-26 08:45:37 -0400

    auto update

--- gitupdate.sh
+++ gitupdate.sh
@@ -2,6 +2,9 @@
 
 #hassio homeassistant check
 
+git config --global user.email "arunrajappan@gmail.com"
+git config --global user.name "vicky4050"
+
 git add .
 git status
 git commit -m "auto update"
